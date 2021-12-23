2b7dd1d977f840d50bb23d18db3a9cb1e6e813bb
EranXu <91049738+EranXu@users.noreply.github.com>
2021-12-23 00:19:15 -0800

    modify the field of phone

--- take-homeAssignment/api/models.py
+++ take-homeAssignment/api/models.py
@@ -8,13 +8,11 @@ from phonenumber_field.modelfields import PhoneNumberField
 # create the table format in database
 class TeamMember(models.Model):
     fullName = models.CharField(null = False, blank=False, max_length=500)
-    phone = PhoneNumberField(null = True)
+    phone = models.CharField(null = True, max_length=200)
     email = models.EmailField(null=False,blank=False,unique=True, max_length=200)
-    canDelete = models.BooleanField(default=False)
+    canDelete = models.BooleanField(default=False, null=True)
     
     def __str__(self):
         return self.fullName
-    
-    class Meta:
-        ordering = ['canDelete']
+   
     
